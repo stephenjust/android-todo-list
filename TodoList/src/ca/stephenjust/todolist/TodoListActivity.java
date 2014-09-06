cd6d0b5d3ae99e9d12dae55b648e739d9c579e14
Stephen Just <stephenjust@gmail.com>
2014-09-06 00:53:55 -0600

    Populate the app's start screen with a dummy list of to-do items.

--- TodoList/src/ca/stephenjust/todolist/TodoListActivity.java
+++ TodoList/src/ca/stephenjust/todolist/TodoListActivity.java
@@ -1,36 +1,71 @@
 package ca.stephenjust.todolist;
 
 import android.app.Activity;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class TodoListActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_list);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo_list);
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new TodoListFragment()).commit();
+		}
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.todo_list, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class TodoListFragment extends ListFragment {
+		TodoList m_list;
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			
+			m_list = new TodoList();
+			m_list.add(new TodoItem("Foo"));
+			m_list.add(new TodoItem("Bar"));
+		}
+		
+		@Override
+		public void onViewCreated(View view, Bundle savedInstanceState) {
+			super.onViewCreated(view, savedInstanceState);
+			
+			setListAdapter(new TodoAdapter(getActivity(), m_list));
+		}
+		
+		@Override
+		public void onDestroyView() {
+			super.onDestroyView();
+			
+			setListAdapter(null);
+		}
+	}
 }
