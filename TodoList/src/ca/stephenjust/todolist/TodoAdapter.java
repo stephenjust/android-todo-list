c87879282f05930786ef16f1005cf3edde4bf0a8
Stephen Just <stephenjust@gmail.com>
2014-09-19 12:14:48 -0600

    Remove redundant Text field in list item view.

--- TodoList/src/ca/stephenjust/todolist/TodoAdapter.java
+++ TodoList/src/ca/stephenjust/todolist/TodoAdapter.java
@@ -10,55 +10,49 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
 
 public class TodoAdapter extends ArrayAdapter<TodoItem> {
 
-	final Context m_context;
-	final List<TodoItem> m_items;
+	final Context mContext;
+	final List<TodoItem> mItems;
 	
 	public TodoAdapter(Context context, List<TodoItem> objects) {
 		super(context, R.layout.list_todo, objects);
-		m_context = context;
-		m_items = objects;
+		mContext = context;
+		mItems = objects;
 	}
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View rowView;
-		LayoutInflater inflater = (LayoutInflater) m_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		if (convertView != null) {
 			rowView = convertView;
 		} else {
 			rowView = inflater.inflate(R.layout.list_todo,  parent, false);
 		}
-		rowView.setLongClickable(true);
 		CheckBox check = (CheckBox) rowView.findViewById(R.id.check_todo);
-		TextView text = (TextView) rowView.findViewById(R.id.text_todo);
 		
-		TodoItem item = m_items.get(position);
-		check.setOnCheckedChangeListener(null); // Clear listener before setting value
-		check.setChecked(item.getCompleted());
+		TodoItem item = mItems.get(position);
 		check.setOnCheckedChangeListener(new TodoCheckboxListener(position));
+		check.setChecked(item.getCompleted());
 		check.setText(item.getText());
-		text.setText(item.getText());
-		text.setVisibility(View.GONE);
 		
 		return rowView;
 	}
 
 	private class TodoCheckboxListener implements OnCheckedChangeListener {
 
-		int m_position;
+		int mPosition;
 
 		public TodoCheckboxListener(int position) {
-			m_position = position;
+			mPosition = position;
 		}
 
 		@Override
 		public void onCheckedChanged(CompoundButton buttonView,
 				boolean isChecked) {
-			m_items.get(m_position).setCompleted(isChecked);
+			mItems.get(mPosition).setCompleted(isChecked);
 		}
 	}
 }
